700bfa3c045c7ae2fe6a34f8fa013cb45f3e6430
Jhonathan Moraes <jhonathanmc04@gmail.com>
2023-10-01 01:42:54 -0300

    Revert ""
    This reverts commit 764df552256013891bf3bac2e78a8c4ffedfd00b.

--- src/main/java/br/com/projetoweb/produto/controllers/PageController.java
+++ src/main/java/br/com/projetoweb/produto/controllers/PageController.java
@@ -3,6 +3,10 @@ package br.com.projetoweb.produto.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ *
+ * @author jhona
+ */
 @Controller
 public class PageController {
 
